4100240598688ec7aee84ad0943e21a672b0d6b3
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-06 22:09:15 +0200

    Added common types

--- Package/Source/EasyIpCommonTypes.pas
+++ Package/Source/EasyIpCommonTypes.pas
@@ -5,6 +5,20 @@ interface
 type
   TByteArray = array of Byte;
 
+  bool = Boolean;
+
+  sbyte = ShortInt;
+
+  short = Smallint;
+
+  int = Integer;
+
+  ushort = Word;
+
+  uint = LongWord;
+
+  float = Single;
+
 implementation
 
 end.
