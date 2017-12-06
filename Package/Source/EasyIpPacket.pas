854f2bd348697d8ada90ae35d95e44b48a8ae4ad
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-06 22:11:30 +0200

    Created EasyIp record (like struct)

--- Package/Source/EasyIpPacket.pas
+++ Package/Source/EasyIpPacket.pas
@@ -2,6 +2,26 @@ unit EasyIpPacket;
 
 interface
 
+type
+  PEasyIpPacket = ^TEasyIpPacket;
+
+  TEasyIpPacket = packed record
+    Flags: Byte;
+    Error: Byte;
+    Counter: Integer;
+    Spare1: Byte;
+    SendDataType: Byte;
+    SendDataSize: Word;
+    SendDataOffset: Word;
+    Spare2: Byte;
+    RequestDataType: Byte;
+    RequestDataSize: Word;
+    RequestDataOffsetServer: Word;
+    RequestDataOffsetClient: Word;
+    Data: array[1..256] of Word;
+  end;
+
 implementation
 
 end.
+
