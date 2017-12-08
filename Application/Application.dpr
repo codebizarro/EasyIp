06c70b099c569639640792f0472d1a3af0db271a
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-08 15:08:03 +0200

    Added stub application

---
+++ Application/Application.dpr
@@ -0,0 +1,15 @@
+program Application;
+{$APPTYPE CONSOLE}
+uses
+  SysUtils,
+  EasyIpChannel in '..\Package\Source\EasyIpChannel.pas',
+  EasyIpCient in '..\Package\Source\EasyIpCient.pas',
+  EasyIpCommonTypes in '..\Package\Source\EasyIpCommonTypes.pas',
+  EasyIpConstants in '..\Package\Source\EasyIpConstants.pas',
+  EasyIpExceptions in '..\Package\Source\EasyIpExceptions.pas',
+  EasyIpHelpers in '..\Package\Source\EasyIpHelpers.pas',
+  EasyIpPacket in '..\Package\Source\EasyIpPacket.pas';
+
+begin
+  // Insert user code here
+end.
