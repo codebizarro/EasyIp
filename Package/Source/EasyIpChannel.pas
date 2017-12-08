510422c4ff1e36b0a86c18cf9d28be496a73a2e1
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-08 14:40:31 +0200

    Created Mock Channel

--- Package/Source/EasyIpChannel.pas
+++ Package/Source/EasyIpChannel.pas
@@ -10,7 +10,41 @@ type
     function Execute(buffer: TByteArray): TByteArray;
   end;
 
+  TMockChannel = class(TInterfacedObject, IChannel)
+  private
+    FHost: string;
+    FPort: int;
+  protected
+  public
+    constructor Create(host: string; port: int); overload;
+    destructor Destroy; override;
+    function Execute(buffer: TByteArray): TByteArray;
+  end;
+
 implementation
+
+constructor TMockChannel.Create(host: string; port: int);
+begin
+  inherited Create;
+  FHost := host;
+  FPort := port;
+end;
+
+destructor TMockChannel.Destroy;
+begin
+  inherited;
+
+end;
+
+function TMockChannel.Execute(buffer: TByteArray): TByteArray;
+var
+  temp: TByteArray;
+begin
+  SetLength(temp, Length(buffer));
+  temp := Copy(buffer, 0, Length(buffer));
+  temp[0] := $80;
+  Result := temp;
+end;
 
 end.
 
