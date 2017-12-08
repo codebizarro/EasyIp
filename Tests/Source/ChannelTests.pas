9a50ada61d35bf76a2afe322ba6dba37fb2cca82
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-08 14:42:35 +0200

    Added channel test with mocked channel

--- Tests/Source/ChannelTests.pas
+++ Tests/Source/ChannelTests.pas
@@ -2,7 +2,63 @@ unit ChannelTests;
 
 interface
 
+uses
+  Windows,
+  SysUtils,
+  Classes,
+  EasyIpCommonTypes,
+  EasyIpConstants,
+  EasyIpPacket,
+  EasyIpHelpers,
+  EasyIpChannel,
+  TestFramework;
+
+type
+  TChannelTest = class(TTestCase)
+  private
+    FChannel: IChannel;
+    FSendPacket: TEasyIpPacket;
+    FSendBuffer: TByteArray;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestExecute;
+  end;
+
 implementation
+
+{ TChannelTest }
+
+procedure TChannelTest.SetUp;
+begin
+  inherited;
+  FChannel := TMockChannel.Create('127.0.0.1', EASYIP_PORT);
+  FSendPacket := TPacketFactory.GetReadPacket(2, EASYIP_TYPE_FLAGWORD, 4);
+  FSendBuffer := TPacketAdapter.ToByteArray(FSendPacket);
+end;
+
+procedure TChannelTest.TearDown;
+begin
+  inherited;
+
+end;
+
+procedure TChannelTest.TestExecute;
+var
+  receiveBuffer: TByteArray;
+  receivePacket: TEasyIpPacket;
+begin
+  receiveBuffer := FChannel.Execute(FSendBuffer);
+
+  Check(receiveBuffer <> nil);
+  Check(Length(FSendBuffer) = Length(receiveBuffer));
+
+  receivePacket := TPacketAdapter.ToEasyIpPacket(receiveBuffer);
+end;
+
+initialization
+  TestFramework.RegisterTest(TChannelTest.Suite);
 
 end.
 
