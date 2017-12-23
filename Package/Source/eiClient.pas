1d2b863157e5c05a350de72b9ca7476ee836852f
Artem.Rudenko <Artem.Rudenko@localhost>
2017-12-23 19:23:51 +0200

    Created skeleton of EasyIpClient

--- Package/Source/eiClient.pas
+++ Package/Source/eiClient.pas
@@ -2,6 +2,78 @@ unit eiClient;
 
 interface
 
+uses
+  eiTypes,
+  eiConstants,
+  eiExceptions,
+  eiProtocol,
+  eiHelpers,
+  eiChannel,
+  Classes,
+  SysUtils,
+  Windows;
+
+type
+  TEasyIpClient = class(TInterfacedObject, IEasyIpProtocol, IEasyIpChannel, IEasyIpClient)
+  private
+    FProtocol: IEasyIpProtocol;
+    FChannel: IEasyIpChannel;
+    FHost: string;
+    function GetChannel: IEasyIpChannel;
+    function GetProtocol: IEasyIpProtocol;
+    property Channel: IEasyIpChannel read GetChannel implements IEasyIpChannel;
+    property Protocol: IEasyIpProtocol read GetProtocol implements IEasyIpProtocol;
+  protected
+  public
+    constructor Create;
+    destructor Destroy; override;
+    function BlockRead(offset: short; dataType: DataTypeEnum; length: byte): DynamicWordArray;
+    procedure BlockWrite(offset: short; value: DynamicWordArray; dataType: DataTypeEnum);
+  published
+    property Host: string read FHost write FHost;
+  end;
+
 implementation
 
+function TEasyIpClient.BlockRead(offset: short; dataType: DataTypeEnum;
+  length: byte): DynamicWordArray;
+begin
+
+end;
+
+procedure TEasyIpClient.BlockWrite(offset: short; value: DynamicWordArray;
+  dataType: DataTypeEnum);
+begin
+
+end;
+
+constructor TEasyIpClient.Create;
+begin
+  inherited;
+  // TODO: create
+end;
+
+destructor TEasyIpClient.Destroy;
+begin
+  inherited;
+  // TODO: destroy
+end;
+
+function TEasyIpClient.GetChannel: IEasyIpChannel;
+begin
+  if Assigned(FChannel) then
+    Result := FChannel
+  else
+    Result := TEasyIpChannel.Create;
+end;
+
+function TEasyIpClient.GetProtocol: IEasyIpProtocol;
+begin
+  if Assigned(FProtocol) then
+    Result := FProtocol
+  else
+    Result := TEasyIpProtocol.Create(pmRead);
+end;
+
 end.
+
--- Tests/Source/ClientTests.pas
+++ Tests/Source/ClientTests.pas
@@ -2,6 +2,60 @@ unit ClientTests;
 
 interface
 
+uses
+  Windows,
+  SysUtils,
+  Classes,
+  eiTypes,
+  eiConstants,
+  eiExceptions,
+  eiProtocol,
+  eiHelpers,
+  eiChannel,
+  eiClient,
+  TestFramework,
+  TestExtensions;
+
+type
+  TClientTest = class(TTestCase)
+  private
+    FClient: IEasyIpClient;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestBlockRead;
+    procedure TestBlockWrite;
+  end;
+
 implementation
 
+{ TClientTest }
+
+procedure TClientTest.SetUp;
+begin
+  inherited;
+  FClient := TEasyIpClient.Create();
+end;
+
+procedure TClientTest.TearDown;
+begin
+  inherited;
+  FClient := nil;
+end;
+
+procedure TClientTest.TestBlockRead;
+begin
+
+end;
+
+procedure TClientTest.TestBlockWrite;
+begin
+
+end;
+
+initialization
+  TestFramework.RegisterTest(TRepeatedTest.Create(TClientTest.Suite, 1));
+
 end.
+
