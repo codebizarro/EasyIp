7e1b4c89f7134d3310f9aee17d4e46d4a51bfff5
Artem.Rudenko <Artem.Rudenko@localhost>
2018-01-16 22:15:32 +0200

    Added StopWatch class utility

--- Tests/Source/UnitTests.pas
+++ Tests/Source/UnitTests.pas
@@ -9,7 +9,10 @@ uses
   eiConstants,
   eiProtocol,
   eiHelpers,
-  TestFramework;
+  TestFramework,
+  TestExtensions,
+  StopWatch,
+  Windows;
 
 type
   TPacketFactoryTest = class(TTestCase)
@@ -21,6 +24,16 @@ type
   published
     procedure TestGetReadPacket;
     procedure TestGetWritePacket;
+  end;
+
+  TStopWatchTest = class(TTestCase)
+  private
+    FStopWatch: IStopWatch;
+  protected
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure TestRun;
   end;
 
 implementation
@@ -53,8 +66,46 @@ begin
   writePacket := TPacketFactory.GetWritePacket(0, EASYIP_TYPE_FLAGWORD, EASYIP_DATASIZE - 1);
 end;
 
+{ TStopWatchTest }
+
+procedure TStopWatchTest.SetUp;
+begin
+  inherited;
+  FStopWatch := TStopWatch.Create;
+end;
+
+procedure TStopWatchTest.TearDown;
+begin
+  inherited;
+  FStopWatch := nil;
+end;
+
+procedure TStopWatchTest.TestRun;
+const
+  startSleepTime = 1;
+  multiplier = 2;
+  stopSleepTime = 10000;
+var
+  delta: int;
+  sleepValue: int;
+begin
+  sleepValue := startSleepTime;
+  while (sleepValue < stopSleepTime) do
+  begin
+    FStopWatch.Start;
+    Sleep(sleepValue);
+    FStopWatch.Stop;
+    delta := FStopWatch.ElapsedMilliseconds - sleepValue;
+    OutputDebugString(PChar(FStopWatch.Elapsed));
+    Check(Abs(delta) < 15);
+//    OutputDebugString(PChar('delta ' + IntToStr(delta)));
+    sleepValue := sleepValue * multiplier;
+  end;
+end;
+
 initialization
   TestFramework.RegisterTest(TPacketFactoryTest.Suite);
+  TestFramework.RegisterTest(TRepeatedTest.Create(TStopWatchTest.Suite, 2));
 
 end.
 
