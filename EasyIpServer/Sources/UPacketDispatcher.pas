99d9bbecf5ab62c0584c12c8147ef83be7f83375
Artem.Rudenko <Artem.Rudenko@localhost>
2018-02-15 10:14:48 +0200

    [EasySrv][PacketDispatcher]: Created stubs for packet type handlers

--- EasyIpServer/Sources/UPacketDispatcher.pas
+++ EasyIpServer/Sources/UPacketDispatcher.pas
@@ -5,15 +5,19 @@ interface
 uses
   eiTypes,
   eiHelpers,
+  eiProtocol,
   UServerTypes;
 
 type
   TPacketDispatcher = class(TInterfacedObject, IPacketDispatcher)
   private
     FLogger: ILogger;
+    function ProcessBitPacket(protocol: IEasyIpProtocol): EasyIpPacket;
+    function ProcessDataPacket(protocol: IEasyIpProtocol): EasyIpPacket;
+    function ProcessInfoPacket(protocol: IEasyIpProtocol): EasyIpPacket;
   public
-    function Process(packet: DynamicByteArray): DynamicByteArray;
     constructor Create(logger: ILogger);
+    function Process(packet: DynamicByteArray): DynamicByteArray;
   end;
 
 implementation
@@ -27,13 +31,42 @@ end;
 function TPacketDispatcher.Process(packet: DynamicByteArray): DynamicByteArray;
 var
   returnPacket: DynamicByteArray;
+  eInPacket, eOutPacket: EasyIpPacket;
+  protocol: IEasyIpProtocol;
   len: int;
 begin
   FLogger.Log('Packet dispatching ...');
+
   //TODO: To implement packet dispatching
-  len := Length(packet);
-  SetLength(returnPacket, len);
-  Result := returnPacket;
+  eInPacket := TPacketAdapter.ToEasyIpPacket(packet);
+  protocol := TEasyIpProtocol.Create(eInPacket);
+
+  if (protocol.Mode = pmRead) or (protocol.Mode = pmWrite) then
+    eOutPacket := ProcessDataPacket(protocol)
+  else if protocol.Mode = pmInfo then
+    eOutPacket := ProcessInfoPacket(protocol)
+  else if (protocol.Mode = pmBit) then
+    eOutPacket := ProcessBitPacket(protocol);
+
+  Result := TPacketAdapter.ToByteArray(eOutPacket);
+end;
+
+function TPacketDispatcher.ProcessBitPacket(protocol: IEasyIpProtocol): EasyIpPacket;
+begin
+  //TODO: To implement packet dispatching
+  Result := protocol.Packet;
+end;
+
+function TPacketDispatcher.ProcessDataPacket(protocol: IEasyIpProtocol): EasyIpPacket;
+begin
+  //TODO: To implement packet dispatching
+  Result := protocol.Packet;
+end;
+
+function TPacketDispatcher.ProcessInfoPacket(protocol: IEasyIpProtocol): EasyIpPacket;
+begin
+  //TODO: To implement packet dispatching
+  Result := protocol.Packet;
 end;
 
 end.
